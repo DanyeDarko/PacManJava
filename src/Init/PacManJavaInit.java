70c741acd4b406d2064f5fb451c08613d7845995
Danye <danielbrzen@outlook.com>
2020-01-30 15:50:58 -0600

    Clase Principal Init del Juego

---
+++ src/Init/PacManJavaInit.java
@@ -0,0 +1,15 @@
+
+package Init;
+
+
+public class PacManJavaInit {
+
+    public static void main(String[] args) {
+
+     TableroDeJuego juegoNuevo = new TableroDeJuego();
+     System.out.println("INICIANDO TABLERO DE JUEGO");
+     juegoNuevo.show(true);
+     
+    }
+    
+}
